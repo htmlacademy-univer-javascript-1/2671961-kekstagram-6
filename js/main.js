a3109eb03edbb746d96bb301ccec47b093eb9b11
Анна Попова <pam-1-ap@yandex.ru>
2025-10-09 18:39:21 +0500

    делит код на ES2015 модули

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { photosArray } from './data.js';
+export { photosArray };
+
+console.log(photosArray);
---
+++ js/util.js
@@ -0,0 +1,11 @@
+// Генерация случайного числа в диапазоне
+const getRandomInteger = (minValue, maxValue) => {
+  const min = Math.ceil(Math.min(minValue, maxValue));
+  const max = Math.floor(Math.max(minValue, maxValue));
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Получение случайного элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export { getRandomInteger, getRandomArrayElement };
