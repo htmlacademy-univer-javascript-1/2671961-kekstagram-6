1ab0f290caa5f8c6bc6cf7ddbfd61dafee3f733b
Keks <keks@htmlacademy.ru>
2025-10-11 12:59:54 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,4 +1,7 @@
-import { photosArray } from './data.js';
-export { photosArray };
+import { generatePhotosArray } from './photos.js';
+
+const photosArray = generatePhotosArray();
 
 console.log(photosArray);
+
+export { photosArray };
