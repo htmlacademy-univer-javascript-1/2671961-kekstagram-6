361fbac73bf61c6a42962bb52e180750c661df0b
Keks <keks@htmlacademy.ru>
2025-11-11 18:15:06 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/functions.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+
+// Проверяет, можно ли провести встречу в рабочее время
+function canMeeting(workStart, workEnd, meetingStart, meetingDuration) {
+  // Преобразует время в количество минут
+  function toMinutes(time) {
+    const [h, m] = time.split(':').map(Number);
+    return h * 60 + m;
+  }
+
+  // Конвертирует все времена в минуты
+  const workStartMin = toMinutes(workStart);
+  const workEndMin = toMinutes(workEnd);
+  const meetingStartMin = toMinutes(meetingStart);
+  const meetingEndMin = meetingStartMin + meetingDuration;
+
+  // Проверяем, что начало раб дня <= встреча <= конец раб дня
+  return meetingStartMin >= workStartMin && meetingEndMin <= workEndMin;
+}
